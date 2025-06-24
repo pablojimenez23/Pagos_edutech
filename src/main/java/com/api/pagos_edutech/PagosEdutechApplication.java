ca5a2473e42248c4227447f1c8a2db28dbf79450
Pablo <pp-pjimenez@zonar.cl>
2025-06-23 21:38:42 -0400

    Espero que funcione

--- src/main/java/com/api/pagos_edutech/PagosEdutechApplication.java
+++ src/main/java/com/api/pagos_edutech/PagosEdutechApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.api.pagos_edutech")
 public class PagosEdutechApplication {
 
 	public static void main(String[] args) {
